f722b27b86f3120ce6f5b11b421cc552dcfab92e
Tobias Hey <tobias.hey@kit.edu>
2017-02-02 13:52:51 +0100

    implemented context hierarchie eval

--- src/test/java/edu/kit/ipd/parse/contextanalyzer/ContextAnalyzerTest.java
+++ src/test/java/edu/kit/ipd/parse/contextanalyzer/ContextAnalyzerTest.java
@@ -277,9 +277,65 @@ public class ContextAnalyzerTest {
 	}
 
 	@Test
+	public void conceptHierarchieBuildingEval() {
+		int total = 0;
+		int tp = 0;
+		int fp = 0;
+		int fn = 0;
+		List<String> failures = new ArrayList<>();
+		for (String id : evalTexts.keySet()) {
+			ppd = new PrePipelineData();
+			List<Pair<String, String>> text = evalTexts.get(id);
+			String input = prepareInputString(text);
+			ppd.setMainHypothesis(StringToHypothesis.stringToMainHypothesis(input));
+			System.out.println(id);
+			executePreviousStages(ppd);
+			try {
+				Context prev = new Context();
+				Context result = new Context();
+				IGraph graph = ppd.getGraph();
+				wsd.setGraph(ppd.getGraph());
+				wsd.exec();
+				graph = wsd.getGraph();
+				do {
+					prev = result;
+					contextAnalyzer.setGraph(graph);
+					contextAnalyzer.exec();
+					result = contextAnalyzer.getContext();
+					graph = contextAnalyzer.getGraph();
+					System.out.println(result.getEntities());
+					System.out.println(result.getActions());
+					System.out.println(result.getConcepts());
+				} while (!prev.equals(result));
+				HashMap<Integer, Integer> indexMap = produceIndexMappings(text, graph);
+				Pair<List<String>, int[]> checkResult = evaluateConceptHierarchieBuilding(text, result, indexMap);
+				total += checkResult.getRight()[0];
+				tp += checkResult.getRight()[1];
+				fp += checkResult.getRight()[2];
+				fn += checkResult.getRight()[3];
+				for (String failure : checkResult.getLeft()) {
+					failures.add(id + ": " + failure);
+				}
+			} catch (MissingDataException e) {
+				e.printStackTrace();
+			}
+		}
+		double precision = (double) tp / (double) (tp + fp);
+		double recall = (double) tp / (double) (tp + fn);
+		double f1 = (2 * precision * recall) / (precision + recall);
+		System.out.println("----------------------------------------------------");
+		System.out.println("| Correct Relations: " + tp + "/" + total + " | Additionally Detected: " + fp + "|");
+		System.out.println("| Precision = " + precision + "   Recall = " + recall + "  F1 = " + f1 + "|");
+		System.out.println("----------------------------------------------------");
+		for (String string : failures) {
+			System.out.println(string);
+		}
+	}
+
+	@Test
 	public void multiple() {
 		ppd = new PrePipelineData();
-		List<Pair<String, String>> text = evalTexts.get("s7p03");
+		List<Pair<String, String>> text = evalTexts.get("s6p07");
 		String input = prepareInputString(text);
 		ppd.setMainHypothesis(StringToHypothesis.stringToMainHypothesis(input));
 		executePreviousStages(ppd);
@@ -301,9 +357,13 @@ public class ContextAnalyzerTest {
 				System.out.println(result.getConcepts());
 			} while (!prev.equals(result));
 			HashMap<Integer, Integer> indexMap = produceIndexMappings(text, graph);
-			int[] res = evaluateConceptBuilding(text, result, indexMap).getRight();
+			Pair<List<String>, int[]> resultEval = evaluateConceptHierarchieBuilding(text, result, indexMap);
+			int[] res = resultEval.getRight();
 			System.out.println(
 					"Total: " + res[0] + ", truePositive: " + res[1] + ", falsePositive: " + res[2] + ", falseNegative: " + res[3]);
+			for (String string : resultEval.getLeft()) {
+				System.out.println(string);
+			}
 		} catch (MissingDataException e) {
 			e.printStackTrace();
 		}
@@ -403,6 +463,10 @@ public class ContextAnalyzerTest {
 
 						fn++;
 					}
+				} else {
+					String failure = "Word:" + word + " at position " + position + " is not connected with any Entity or Action!";
+					failures.add(failure);
+					fn++;
 				}
 			}
 		}
@@ -439,6 +503,111 @@ public class ContextAnalyzerTest {
 					}
 				}
 			}
+		}
+		return new Pair<List<String>, int[]>(failures, new int[] { total, tp, fp, fn });
+
+	}
+
+	private Pair<List<String>, int[]> evaluateConceptHierarchieBuilding(List<Pair<String, String>> text, Context result,
+			HashMap<Integer, Integer> indexMap) {
+		int tp = 0;
+		int fp = 0;
+		int fn = 0;
+		int total = 0;
+		List<String> failures = new ArrayList<>();
+		HashSet<AbstractConcept> alreadyChecked = new HashSet<>();
+		for (int i = 0; i < text.size(); i++) {
+			Pair<String, String> pair = text.get(i);
+			String word = pair.getLeft();
+			String annotation = pair.getRight();
+			if (annotation != null && indexMap.containsKey(i)) {
+
+				int position = indexMap.get(i);
+				ContextIndividual ci = getContainingContextIndividual(position, result);
+				if (ci instanceof Entity) {
+					AbstractConcept concept = ContextUtils.getMostLikelyEntityConcept(ci.getRelations());
+					AbstractConcept last = concept;
+					AbstractConcept start = concept;
+
+					if (concept != null) {
+						if (!alreadyChecked.contains(concept)) {
+							String content = annotation.substring(1, annotation.length() - 1).trim();
+
+							String[] annotationContent = content.split(",");
+							if (annotationContent.length > 1) {
+								total += annotationContent.length - 1;
+								if (concept.getName().toLowerCase().equals(annotationContent[0].toLowerCase())) {
+									//TODO: Be able to check mutiple super concepts
+									for (int j = 1; j < annotationContent.length; j++) {
+										String name = annotationContent[j];
+										for (AbstractConcept superConcept : concept.getSuperConcepts()) {
+											if (superConcept.getName().equalsIgnoreCase(name)) {
+												tp++;
+												concept = superConcept;
+
+											} else {
+												String failure = "Concept: " + concept.getName() + " has SuperConcept " + superConcept
+														+ " which was not expected";
+												failures.add(failure);
+												fp++;
+											}
+										}
+										if (concept.equals(last)) {
+											String failure = "Concept: " + concept.getName() + " has no SuperConcept " + name;
+											failures.add(failure);
+
+											fn++;
+										} else {
+											alreadyChecked.add(concept);
+											last = concept;
+										}
+									}
+								} else {
+									String failure = "Word:" + word + " at position " + position + " has related Concept: "
+											+ concept.getName() + " but Concept " + annotationContent[0] + " was expected";
+									failures.add(failure);
+
+									fn += annotationContent.length - 1;
+								}
+
+							} else {
+								if (!concept.getName().toLowerCase().equals(annotationContent[0].toLowerCase())) {
+
+									String failure = "Word:" + word + " at position " + position + " has related Concept: "
+											+ concept.getName() + " but Concept " + annotationContent[0] + " was expected";
+									failures.add(failure);
+
+									fn++;
+									fp++;
+								}
+							}
+
+						}
+					} else {
+
+						String failure = "Word:" + word + " at position " + position + " has no related Concept!";
+						failures.add(failure);
+
+						fn++;
+					}
+					alreadyChecked.add(start);
+				} else if (ci instanceof Action) {
+
+				}
+			}
+		}
+		for (AbstractConcept abstractConcept : result.getConcepts()) {
+			if (!alreadyChecked.contains(abstractConcept) && abstractConcept.hasSuperConcepts()) {
+				for (AbstractConcept concept : abstractConcept.getSuperConcepts()) {
+					String failure = "Concept:" + abstractConcept.getName() + " has SuperConcept " + concept.getName()
+							+ " but no SuperConcept was expected";
+					failures.add(failure);
+
+					fp++;
+					alreadyChecked.add(concept);
+				}
+			}
+
 		}
 		return new Pair<List<String>, int[]>(failures, new int[] { total, tp, fp, fn });
 
